06a3475ec8b393dde203e2c393c20a99df61e179
se-jo-ma <46336194+se-jo-ma@users.noreply.github.com>
2026-06-25 11:50:54 -0400

    chore(main): release 0.5.3 (#160)

--- src/stargraph/__init__.py
+++ src/stargraph/__init__.py
@@ -69,7 +69,7 @@ def __getattr__(name: str) -> Any:
     raise AttributeError(msg)
 
 
-__version__ = "0.5.2"  # x-release-please-version
+__version__ = "0.5.3"  # x-release-please-version
 
 __all__ = [
     "DocStore",
